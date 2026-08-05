3a0cba4065a3f82d887b66c4ca9488a66a1938c6
YashVerma-0024 <yashverma29082008@gmail.com>
2026-08-05 20:34:18 +0530

    121. Sell and Buy

---
+++ 121. Sell & Buy.py
@@ -0,0 +1,19 @@
+prices = [7, 6, 4, 0, 3, 1]
+
+
+def maxProfit(prices):
+    if not prices:
+        return 0
+
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices[1:]:
+        if price < min_price:
+            min_price = price
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+
+print(maxProfit(prices))
