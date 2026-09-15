e60c532bfe819899704b86a0f3944ee5d4cc4e06
YashVerma-0024 <yashverma29082008@gmail.com>
2026-09-15 19:37:28 +0530

    33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -5,22 +5,10 @@
 def search(nums: list[int], target: int) -> int:
     n = len(nums)
 
-    # case 1 : target not in nums
-    is_present = False
     for i in range(n):
         if nums[i] == target:
-            is_present = True
-            break
-
-    if is_present == False:
-        return -1
-
-    # Case 2 : target is present in nums
-    for j in range(n):
-        if nums[j] == target:
-            return j
-    
-
+            return i
+    return -1
 
 
 ##########################################################################
@@ -29,15 +17,24 @@ def search(nums: list[int], target: int) -> int:
 
 def search(nums: list[int], target: int) -> int:
     n = len(nums)
-    ub, lb = -1,-1
     low,high = 0,n-1
+    while low <= high:
+        mid = (low + high)//2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] <= nums[high]:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            if nums[low] <= target <= nums[high]:
+                high = mid - 1
+            else:
+                low = mid + 1
 
-    for i in range(n):
-        mid = (low+high)//2
-        if nums[mid] < target:
-            lb
-
+    return -1
 
 nums = [4,5,6,7,0,1,2]
-target = 0
+target = 1
 print(search(nums, target))
